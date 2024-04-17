8b19eaee6612768dbe047ea02857321b9191d56a
balandzin <balandzin2602@gmail.com>
2024-04-17 17:22:43 +0300

    initial commit

---
+++ FitGuide/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  FitGuide
+//
+//  Created by Антон Баландин on 17.04.24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
