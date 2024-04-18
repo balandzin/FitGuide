128356468d36107bcfdbaa9d8b31ff1f6fc1d171
balandzin <balandzin2602@gmail.com>
2024-04-18 22:27:40 +0300

    Добавил FirstViewController

---
+++ FitGuide/FirstViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FirstViewController.swift
+//  FitGuide
+//
+//  Created by Антон Баландин on 18.04.24.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
