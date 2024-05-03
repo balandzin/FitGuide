be09fa171cff1263d701b9c898f4c555f7ea9a12
balandzin <balandzin2602@gmail.com>
2024-05-03 14:35:23 +0300

    Произвел расчет индекса массы тела

--- FitGuide/AuthorizationViewController.swift
+++ FitGuide/AuthorizationViewController.swift
@@ -51,6 +51,8 @@ final class AuthorizationViewController: UIViewController {
         button.setTitle("Calculate body mass index", for: .normal)
         button.backgroundColor = .blue
         button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(calculateBMIButtonTapped), for: .touchUpInside)
+        
 //        // Добавляем границу кнопки
 //        button.layer.borderWidth = 2
 //        button.layer.borderColor = UIColor.darkGray.cgColor
@@ -63,6 +65,12 @@ final class AuthorizationViewController: UIViewController {
     }()
     
     private lazy var bodyMassIndexLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        return label
+    } ()
+    
+    private lazy var idealBodyMassIndexLabel: UILabel = {
         let label = UILabel()
         label.numberOfLines = 0
         return label
@@ -92,51 +100,108 @@ final class AuthorizationViewController: UIViewController {
         view.addSubview(heightTextField)
         view.addSubview(calculateBMIButton)
         view.addSubview(bodyMassIndexLabel)
+        view.addSubview(idealBodyMassIndexLabel)
         view.addSubview(trainProgramButton)
         
         setupConstraints()
     }
     
+    @objc private func calculateBMIButtonTapped() {
+        if nameTextField.text == "" || weightTextField.text == "" || heightTextField.text == "" {
+            showAlert(message: "Введите корректные данные пользователя")
+        } else {
+            bodyMassIndexLabel.text = """
+            \(nameTextField.text ?? ""),  ваш индекс массы тела:
+            \(getBodyMassIndex().0) - \(getBodyMassIndex().1)
+            """
+            
+            if genderSegmentedControl.selectedSegmentIndex == 0 {
+                idealBodyMassIndexLabel.text = "Идеальный индекс массы тела для мужчины равен 23"
+            } else {
+                idealBodyMassIndexLabel.text = "Идеальный индекс массы тела для женщины равен 21.5"
+            }
+        }
+        
+        view.endEditing(true)
+    }
+    
     private func setupConstraints() {
         nameTextField.snp.makeConstraints { make in
             make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
-            //make.centerX.equalToSuperview()
             make.leading.trailing.equalToSuperview().inset(16)
         }
         
         genderSegmentedControl.snp.makeConstraints { make in
             make.top.equalTo(nameTextField.snp.bottom).offset(20)
-            //make.centerX.equalToSuperview()
             make.leading.trailing.equalToSuperview().inset(16)
         }
         
         weightTextField.snp.makeConstraints { make in
             make.top.equalTo(genderSegmentedControl.snp.bottom).offset(20)
-            //make.centerX.equalToSuperview()
             make.leading.trailing.equalToSuperview().inset(16)
         }
         
         heightTextField.snp.makeConstraints { make in
             make.top.equalTo(weightTextField.snp.bottom).offset(20)
-            //make.centerX.equalToSuperview()
             make.leading.trailing.equalToSuperview().inset(16)
         }
         
         calculateBMIButton.snp.makeConstraints { make in
             make.top.equalTo(heightTextField.snp.bottom).offset(20)
-            //make.centerX.equalToSuperview()
             make.leading.trailing.equalToSuperview().inset(16)
+            make.height.equalTo(50)
         }
         
         bodyMassIndexLabel.snp.makeConstraints { make in
             make.top.equalTo(calculateBMIButton.snp.bottom).offset(20)
-            //make.centerX.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(16)
+        }
+        
+        idealBodyMassIndexLabel.snp.makeConstraints { make in
+            make.top.equalTo(bodyMassIndexLabel.snp.bottom).offset(20)
             make.leading.trailing.equalToSuperview().inset(16)
         }
         
         trainProgramButton.snp.makeConstraints { make in
             make.bottom.equalTo(view.safeAreaLayoutGuide).inset(20)
             make.leading.trailing.equalToSuperview().inset(16)
+            make.height.equalTo(50)
+
         }
+    }
+    
+    private func getBodyMassIndex() -> (Double, String) {
+        var conclusion = ""
+        let heigh = Double(heightTextField.text ?? "1") ?? 1
+        let weight = Double(weightTextField.text ?? "0") ?? 0
+        
+        
+        let bodyMassIndex = weight / (heigh * heigh / 10000)
+        let roundedIndex = round(bodyMassIndex * 10)/10
+        
+        switch bodyMassIndex {
+        case ..<16: conclusion = "Выраженный дефицит массы тела"
+        case 16..<18.5: conclusion = "Недостаточная масса тела (дефицит)"
+        case 18.5..<25: conclusion = "Норма"
+        case 25..<30: conclusion = "Избыточная масса тела (состояние, предшествующее ожирению)"
+        case 30..<35: conclusion = "Ожирение 1-й степени"
+        case 35..<40: conclusion = "Ожирение 2-й степени"
+        default:
+            conclusion = "Ожирение 3-й степени"
+        }
+        
+        return (roundedIndex, conclusion)
+    }
+    
+    private func showAlert(message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
 }
