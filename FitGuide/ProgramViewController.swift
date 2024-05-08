8846a9385bf2dbac366d0a45c7085630c9acbf20
balandzin <balandzin2602@gmail.com>
2024-05-08 14:24:33 +0300

    Сделал таблицу с тренировками

--- FitGuide/ProgramViewController.swift
+++ FitGuide/ProgramViewController.swift
@@ -7,16 +7,76 @@
 
 import UIKit
 
-class ProgramViewController: UIViewController {
+final class ProgramViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Программа тренировок"
         
-        view.backgroundColor = .white
+        tableView = UITableView(frame: view.bounds, style: .plain)
+        
+        // Назначьте текущий класс в качестве источника данных и делегата для таблицы
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        // Добавьте таблицу на экран
+        view.addSubview(tableView)
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "exercise")
+    }
 
-        let exercisesData = Bundle.main.decode(ExercisesData.self, from: "data.json")
-        print(exercisesData)
+}
+
+extension ProgramViewController {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let program = Exercises.getProgram()
         
+        let cell = tableView.dequeueReusableCell(withIdentifier: "exercise", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        
+        
+        if indexPath.row == 0 {
+            content.text = program[indexPath.section].exercises1
+            content.image = UIImage(systemName: "figure.run")
+        } else if indexPath.row == 1 {
+            content.text = program[indexPath.section].exercises2
+            content.image = UIImage(systemName: "dumbbell")
+        } else if indexPath.row == 2 {
+            content.text = program[indexPath.section].exercises3
+            content.image = UIImage(systemName: "dumbbell.fill")
+        } else if indexPath.row == 3 {
+            content.text = program[indexPath.section].exercises4
+            content.image = UIImage(systemName: "dumbbell")
+        } else if indexPath.row == 4 {
+            content.text = program[indexPath.section].exercises5
+            content.image = UIImage(systemName: "dumbbell.fill")
+        } else if indexPath.row == 5 {
+            content.text = program[indexPath.section].exercises6
+            content.image = UIImage(systemName: "figure.cooldown")
+        }
+        
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let program = Exercises.getProgram()
+        return program[section].day
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
+
