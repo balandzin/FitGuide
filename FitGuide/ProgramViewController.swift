0ee3b8d8b6656500d37fd7e723027360173764f2
balandzin <balandzin2602@gmail.com>
2024-05-08 13:51:15 +0300

    Получил данные из Bundle

--- FitGuide/ProgramViewController.swift
+++ FitGuide/ProgramViewController.swift
@@ -8,13 +8,15 @@
 import UIKit
 
 class ProgramViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .white
 
+        let exercisesData = Bundle.main.decode(ExercisesData.self, from: "data.json")
+        print(exercisesData)
+        
     }
-
-
 }
